2d2f95291e1f8498297c4d67035994fa054f7dd6
Wei <linkano.teck@gmail.com>
2021-12-22 10:26:50 +1100

    update bracket scope

--- Creation Pattern/creationPattern.js
+++ Creation Pattern/creationPattern.js
@@ -1,33 +1,35 @@
 // Declaration
-const Developer = function (name) {
-	this.name = name;
-	this.type = 'Developer';
-};
-
-const Tester = function (name) {
-	this.name = name;
-	this.type = 'Tester';
-};
-
-const Factory = function () {
-	this.employees = [];
-
-	return {
-		create: (name, type) => {
-			switch (type) {
-				case 1:
-					this.employees.push(new Developer(name));
-					break;
-				case 2:
-					this.employees.push(new Tester(name));
-					break;
-				default:
-					return console.log('Employee type error');
-			}
-		},
-		getEmployees: () => this.employees,
+{
+	const Developer = function (name) {
+		this.name = name;
+		this.type = 'Developer';
 	};
-};
+
+	const Tester = function (name) {
+		this.name = name;
+		this.type = 'Tester';
+	};
+
+	var Factory = function () {
+		this.employees = [];
+
+		return {
+			create: (name, type) => {
+				switch (type) {
+					case 1:
+						this.employees.push(new Developer(name));
+						break;
+					case 2:
+						this.employees.push(new Tester(name));
+						break;
+					default:
+						return console.log('Employee type error');
+				}
+			},
+			getEmployees: () => this.employees,
+		};
+	};
+}
 
 // Creation and Method Using
 const myFactory = new Factory();
@@ -39,4 +41,5 @@ myFactory.create('Peter', 1);
 myFactory.create('Jason', 2);
 myFactory.create('Willy', 2);
 myFactory.create('Jesse', 1);
+
 console.log(myFactory.getEmployees());
