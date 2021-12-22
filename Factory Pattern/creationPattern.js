ed72fe0ae006721b9b0a20124325f1c76214da4d
Wei <linkano.teck@gmail.com>
2021-12-22 12:59:36 +1100

    Description of Factory Pattern

---
+++ Factory Pattern/creationPattern.js
@@ -0,0 +1,52 @@
+/**
+ * A type of Creational Pattern
+ * In order for increasing object/programe maintainability and reusability
+ * Simulate OOP pattern as module and instance, in ES6 can also be declarated
+ * by using class.
+ */
+
+// Declaration
+{
+	const Developer = function (name) {
+		this.name = name;
+		this.type = 'Developer';
+	};
+
+	const Tester = function (name) {
+		this.name = name;
+		this.type = 'Tester';
+	};
+
+	var Factory = function () {
+		this.employees = [];
+
+		return {
+			create: (name, type) => {
+				switch (type) {
+					case 1:
+						this.employees.push(new Developer(name));
+						break;
+					case 2:
+						this.employees.push(new Tester(name));
+						break;
+					default:
+						return console.log('Employee type error');
+				}
+			},
+			getEmployees: () => this.employees,
+		};
+	};
+}
+
+// Creation and Method Using
+const myFactory = new Factory();
+
+myFactory.create('John', 1);
+myFactory.create('Mary', 1);
+myFactory.create('Stefanie', 2);
+myFactory.create('Peter', 1);
+myFactory.create('Jason', 2);
+myFactory.create('Willy', 2);
+myFactory.create('Jesse', 1);
+
+console.log(myFactory.getEmployees());
