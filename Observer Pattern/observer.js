5675598de46392c27318757de8e47f9b1ecaaf4d
Wei <linkano.teck@gmail.com>
2022-01-11 22:40:04 +1100

    Observer finisher

---
+++ Observer Pattern/observer.js
@@ -0,0 +1,47 @@
+// The Observer pattern is a design pattern that offers a subscription model 
+// in which objects (known as 'observers') can subscribe to an event (known as a 'subject')
+// and get notified when the event occurs (or when the subject sends a signal). 
+
+// Useful to create event handlers
+
+class Subject {
+    constructor() {
+        this.observers = []
+    }
+
+    subscribe(fn) {
+        this.observers.push(fn)
+    }
+
+    unsubcribe(removeFn) {
+        this.observers = this.observers.filter( fn => fn !== removeFn )
+    }
+
+    fire() {
+        this.observers.forEach(fn => fn.call())
+    }
+
+    showList() {
+        console.log(this.observers)
+    }
+}
+
+const observer = new Subject()
+
+const fn1 = function(){
+    console.log('Observer 1 Fires')
+}
+const fn2 = function(){
+    console.log('Observer 2 Fires')
+}
+const fn3 = function(){
+    console.log('Observer 3 Fires')
+}
+
+observer.subscribe(fn1)
+observer.subscribe(fn2)
+observer.subscribe(fn3)
+
+observer.unsubcribe(fn2)
+
+observer.fire()
